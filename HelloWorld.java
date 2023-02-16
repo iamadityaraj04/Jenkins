30e081857db7563bef34e58d807dc9f487019283
Aditya <aditya@Adityas-MacBook-Air.local>
2023-02-16 16:51:29 +0530

    helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello! This program is running through jenkins...");	}
+		System.out.println("Hello! This program is executing through jenkins...");	}
 }
